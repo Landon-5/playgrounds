0f1fc199451f4322c661e2c7cb69458da9abb501
Landon-5 <la.al.howard@gmail.com>
2024-08-20 13:46:27 -0600

    Working on basics of Swift
    Started playgrounds and learning swift information

---
+++ SwiftBasics.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var environment = "develpoment"
+environment = "Coding at CTEC"
+environment = "9"
+let hotdog = "yummy"
+var school: String
+school = "l"
+var red, green, blue: Int
+red = 2
+green = 1
+hotdog
+red = 1
+
