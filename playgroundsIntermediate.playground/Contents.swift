edf4b6bd86cb5857023f58f9bfbbc30056feaac1
Landon-5 <la.al.howard@gmail.com>
2024-08-30 11:53:49 -0600

    Learning "Collection Types" (Arrays, Sets)
    Friday just still trying to learn the basics

--- playgroundsIntermediate.playground/Contents.swift
+++ playgroundsIntermediate.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Collection Types Tab In Swift Basics Website
+var randomThing = "Random thing"
+let skibidi = "skibidi"
+Array(arrayLiteral: greeting, randomThing, skibidi)
+Set(arrayLiteral: greeting, randomThing, skibidi)
+Dictionary
