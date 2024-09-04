6a7016cdf6b89f2787f548fbb5894f449208f194
Landon-5 <la.al.howard@gmail.com>
2024-09-04 12:56:04 -0600

    Learned about arrays
    learned about setting the typing of arrays. You don't have to put it it can just be implied by the info in the array.

--- playgroundsIntermediate.playground/Contents.swift
+++ playgroundsIntermediate.playground/Contents.swift
@@ -3,8 +3,12 @@ import UIKit
 var greeting = "Hello, playground"
 
 //Collection Types Tab In Swift Basics Website
-var randomThing = "Random thing"
-let skibidi = "skibidi"
-Array(arrayLiteral: greeting, randomThing, skibidi)
-Set(arrayLiteral: greeting, randomThing, skibidi)
-Dictionary
+
+var someInts: [Int] = []
+print("someInts is of type [Int] with \(someInts.count) items.")
+// Prints "someInts is of type [Int] with 0 items."
+
+someInts.append(3)
+// someInts now contains 1 value of type Int
+someInts = []
+// SomeInts is now an empty array, but is still of type [Int]
