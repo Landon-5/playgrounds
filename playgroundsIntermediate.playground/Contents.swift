32c8c253b147e8f1786c041dde86293ef6357d82
Landon-5 <la.al.howard@gmail.com>
2024-09-06 11:44:58 -0600

    Tried replicating what we did in class today with the functions and stuff in the algorithmsView

--- playgroundsIntermediate.playground/Contents.swift
+++ playgroundsIntermediate.playground/Contents.swift
@@ -1,14 +1,31 @@
-import UIKit
+import SwiftUI
 
-var greeting = "Hello, playground"
+struct intermediatePlaygroundView: View
+{
+    let title : String = "Juan is a Baka"
+    @State private var backgroundColor : Color = .green
+    var body: some View
+    {
+        var greeting = "Hello, playground"
+        Image(systemname: "globe")
+        //Collection Types Tab In Swift Basics Website
+        
+        var someInts: [Int] = []
+        print("someInts is of type [Int] with \(someInts.count) items.")
+        // Prints "someInts is of type [Int] with 0 items."
+        
+        someInts.append(3)
+        // someInts now contains 1 value of type Int
+        someInts = []
+        // SomeInts is now an empty array, but is still of type [Int]
+    }
+    func juanBaka(with bullet: String) -> String
+    var juanBakaDescription : String = ""
+    
+    
+}
 
-//Collection Types Tab In Swift Basics Website
-
-var someInts: [Int] = []
-print("someInts is of type [Int] with \(someInts.count) items.")
-// Prints "someInts is of type [Int] with 0 items."
-
-someInts.append(3)
-// someInts now contains 1 value of type Int
-someInts = []
-// SomeInts is now an empty array, but is still of type [Int]
+#Preview 
+{
+    intermediatePlaygroundView()
+}
