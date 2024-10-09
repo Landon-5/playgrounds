7fbb360406ded07a840ecd9a18eb5c2b9d30ed3e
Landon-5 <la.al.howard@gmail.com>
2024-10-09 13:09:26 -0600

    Pasted what the teacher gave us into the Json practice

--- JsonPlayground.playground/Contents.swift
+++ JsonPlayground.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let demo = """
+[
+    {
+      "id": "8aed359b-2cbc-49de-82e8-53d87ba602f4",
+      "year": 2017,
+      "goal": "Become more proficient in Java and Swift.",
+      "creature": "warg"
+    },
+    {
+      "id": "ea237f3d-9a23-41c9-956c-1aa29b3bb180",
+      "year": 2017,
+      "goal": "C#",
+      "creature": "Squonk"
+    },
+    {
+      "id": "0ef4b339-fe37-4ce4-80c8-052d067980c9",
+      "year": 2017,
+      "goal": "Climb more",
+      "creature": "thunderbird"
+    },
+    {
+      "id": "ea8acf13-3c38-41bc-83cc-a59e74418921",
+      "year": 2017,
+      "goal": "Exercise",
+      "creature": "unicorn"
+    }
+]
+""".data(using: .utf8)!
