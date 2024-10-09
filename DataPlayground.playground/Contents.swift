2f33d2c3fcd30ce206f6a75c7df7cb8b8ece02c0
Landon-5 <la.al.howard@gmail.com>
2024-10-09 12:25:32 -0600

    Just made 2 lets start and range and just defined the types of them, havent assigned anything into them yet

--- DataPlayground.playground/Contents.swift
+++ DataPlayground.playground/Contents.swift
@@ -5,6 +5,10 @@ var greeting = "Hello, playground"
 func generateRandomEmoji(of emojiType : String) -> String
 {
     let defaultEmoji = "🗣️"
+    let start : Int
+    let range : Int
+    
+    
     
     return defaultEmoji
 }
